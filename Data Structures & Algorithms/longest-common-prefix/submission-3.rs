781e39ac67cbcad535e7ebd64a591af849379b50
Shihab <mdshihabkabir07@gmail.com>
2026-05-18 07:02:00 +0600

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.rs
@@ -0,0 +1,19 @@
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        if strs.is_empty(){return String::new();}
+
+    for (idx, b) in first.as_bytes().iter().enumerate() {
+    let b = *b; // dereference to u8
+    for other in &strs[1..] {
+        // check if other is long enough
+        if idx >= other.as_bytes().len() {
+            return first[..idx].to_string();
+        }
+        // check if byte is different
+        if other.as_bytes()[idx] != b {
+            return first[..idx].to_string();
+        }
+    }
+}
+    }
+}
