c8109a4da90016824e9bd6022bd2b269a92e649c
Shihab <mdshihabkabir07@gmail.com>
2026-05-19 22:44:40 +0600

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut map: HashMap<Vec<char>, Vec<String>> = HashMap::new();
+        for s in strs{
+            let mut key:Vec<char>=s.chars().collect();
+            key.sort();
+            map.entry(key).or_insert(Vec::new().push(s))
+        }
+
+        map.into_value().collect()
+    
+    }
+}
