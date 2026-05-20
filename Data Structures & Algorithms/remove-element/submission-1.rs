88640bdb8bfa896f105232a81a9a202b265a6216
Shihab <mdshihabkabir07@gmail.com>
2026-05-20 08:22:03 +0600

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
+  
+ let k=0;
+
+ for i in 0..nums.len(){
+    if nums[i]!=val{
+       nums[k] = nums[i];
+                k += 1;
+    }
+ }
+ k
+
+}
+}
