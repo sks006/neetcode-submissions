df28e3afdcf6fea78b6f06170812d982ed3bc7b7
Shihab <mdshihabkabir07@gmail.com>
2026-05-21 08:39:50 +0600

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn majority_element(nums: Vec<i32>) -> i32 {
+
+    let count_ref: &mut i32 = counts.entry(num).or_insert(0);
+*count_ref += 1;
+              counts_ref
+        .into_iter()
+        .max_by_key(|&(_num, count)| count)
+        .map(|(num, _)| num)
+        .unwrap()
+    }
+}
