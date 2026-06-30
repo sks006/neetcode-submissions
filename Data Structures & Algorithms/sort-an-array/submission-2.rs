1a975c1d1bd07468d6df7a8e997baa740426ee99
Shihab <mdshihabkabir07@gmail.com>
2026-06-30 22:50:29 +0600

    Add: sort-an-array - submission-2

---
+++ Data Structures & Algorithms/sort-an-array/submission-2.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn sort_array(mut nums: Vec<i32>) -> Vec<i32> {
+   let mut temp = 0;
+     for x in 0..nums.len() {
+        for y in 1..nums.len() {   // full range from 1 to len-1
+            if nums[x] > nums[y] {
+                temp = nums[y];
+                nums[y] = nums[x];
+                nums[x] = temp;
+            }
+        }
+    }
+    nums
+    }
+    
+}
