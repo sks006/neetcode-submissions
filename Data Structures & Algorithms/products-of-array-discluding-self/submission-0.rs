e1538146082ba362ea067226e9f6cae4ac3ee7df
Shihab <mdshihabkabir07@gmail.com>
2026-07-16 22:32:41 +0600

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.rs
@@ -0,0 +1,22 @@
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+      let n= nums.len();
+      let mut result=vec![1;n];
+    
+      
+      let mut prefix=1;
+      for i in 0..n{
+        result[i]=prefix;
+        prefix*=nums[i];
+      }
+
+      let mut postfix=1;
+      for i in (0..n).rev(){
+        result[i] *= postfix; 
+        postfix *=nums[i];
+      }
+
+      result
+
+    }
+}
